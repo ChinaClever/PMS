0b746f8c6c49fe6442e4c79e9abc20df03145b21
zhanlinjie <luozhiyong131@qq.com>
2023-11-15 14:51:53 +0800

    意见反馈模块
    前端和后段代码

--- pmsAdmin/application/settings.py
+++ pmsAdmin/application/settings.py
@@ -116,6 +116,7 @@ INSTALLED_APPS = [
     'application.menu',
     'application.config_web',
     'application.upload',
+    'application.suggestion'
 ]
 
 MIDDLEWARE = [
--- pmsAdmin/application/urls.py
+++ pmsAdmin/application/urls.py
@@ -92,5 +92,6 @@ urlpatterns = [
     path('configweb/', include('application.config_web.urls')),
     # 焊接报表
     path('welding/', include('application.welding.urls')),
-
+    # 意见反馈
+    path('suggestion/', include('application.suggestion.urls')),
 ]
