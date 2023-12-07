3017253f67d812b27cc14d0619cb553185ca455b
luosuofan <luozhiyong131@qq.com>
2023-12-07 14:38:04 +0800

    修改了supplier数据迁移

--- pmsAdmin/application/settings.py
+++ pmsAdmin/application/settings.py
@@ -116,7 +116,7 @@ INSTALLED_APPS = [
     'application.menu',
     'application.config_web',
     'application.upload',
-    #'application.supplier',
+    'application.supplier',
     'application.repairreport',
     'application.suggestion',
     'application.inspectreport',
