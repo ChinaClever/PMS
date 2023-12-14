3f55b9aea3742472f3b235f60596ccc9c5b30b82
Ljy <18775111884@163.com>
2023-12-14 15:32:14 +0800

    增加分析页后端

--- pmsAdmin/application/analysis/services.py
+++ pmsAdmin/application/analysis/services.py
@@ -10,108 +10,87 @@ from django.core.paginator import Paginator, PageNotAnInteger, InvalidPage, Empt
 from django.db.models import Sum
 from utils import R, regular
 
-# 根据工单查询所需要的数据
-def DetailAll(request):
+# 根据产品名称查询所需要的数据
+def RepairData(request):
     # 页码
-    page = int(request.GET.get('page', 1))
+    page = int(request.GET.get("page", 1))
     # 每页数
-    limit = int(request.GET.get('limit', PAGE_LIMIT))
-    shipmentReportS = Shipment.objects.filter(is_delete=False)
-    if not shipmentReportS:
-        return None
-    data = set()
-    # 排序
-    sort = request.GET.get('sort')
-    order = request.GET.get('order')
-    if sort and order:
-        shipmentReportS = shipmentReportS.order_by(f'-{sort}' if order == 'desc' else sort)
+    limit = int(request.GET.get("limit", PAGE_LIMIT))
+    # 实例化查询对象
+    query = Dict.objects.filter(is_delete=False)
+    startTime = request.GET.get('startTime')
+    endTime = request.GET.get('endTime')
+    if startTime and endTime:
+        startTime = startTime.replace("+", " ")
+        endTime = endTime.replace("+", " ")
+        # sql = 'SELECT item_number,sum(examine_an_amount) AS total,sum(examine_a_bad_amount) AS badtotal FROM django_inspectreport WHERE is_delete = 0 AND start_time >= ' + str(startTime) + ' AND end_time <= ' + str(endTime)+ " GROUP BY item_number" + " limit " + str(limit)
+        sql = ("SELECT t1.id, t1.name, sum(t1.repair_number) AS repair_total, sum(t2.examine_an_amount) AS total "
+               "FROM django_repairreport AS t1 "
+               "Join django_inspectreport AS t2 ON t1.name = t2.product_name "
+               "WHERE t1.is_delete = 0 AND t2.is_delete=0 and DATE(create_time) >= %s AND DATE(create_time) <= %s "
+               "GROUP BY name ")
+        query = Dict.objects.raw(sql, [startTime, endTime])
+        # 设置分页
+        paginator = Paginator(query, limit)
     else:
-        shipmentReportS = shipmentReportS.order_by("-id")
-    # 分页设置
-    paginator = Paginator(shipmentReportS, limit)
+
+        sql = ("SELECT t1.id, t1.name, sum(t1.repair_number) AS repair_total, sum(t2.examine_an_amount) AS total "
+               "FROM django_repairreport AS t1 "
+               "Join django_inspectreport AS t2 ON t1.name = t2.product_name "
+               "WHERE t1.is_delete = 0 and t2.is_delete = 0 "
+               "GROUP BY name ")
+        query = Dict.objects.raw(sql)
+        paginator = Paginator(query, limit)
+
     # 记录总数
     count = paginator.count
     # 分页查询
-    try:
-        shipmentReportS = paginator.page(page)
-    except PageNotAnInteger:
-        # 如果请求的页数不是整数, 返回第一页。
-        shipmentReportS = paginator.page(1)
-    except EmptyPage:
-        # 如果请求的页数不在合法的页数范围内，返回结果的最后一页。
-        shipmentReportS = paginator.page(paginator.num_pages)
-    except InvalidPage:
-        # 如果请求的页数不存在, 重定向页面
-        return R.failed('找不到页面的内容')
-
-    if len(shipmentReportS) > 0:
-        for shipmentReport in shipmentReportS:
-            #查询所有的工单号
-            workId = shipmentReport.work_order
-
-            # 质检报表查询质检数量
-            inspectReport = (Inspectreport.objects.filter(is_delete=False, work_order=workId)
-                             .aggregate(inspectReport=Sum('examine_an_amount')))['inspectReport']
-            if inspectReport == None:
-                inspectReport = 0
-
-            # 调试报表的调试数量
-            debugReport = (Debug.objects.filter(is_delete=False, work_order=workId)
-                        .aggregate(debugReport=Sum('product_count')))['debugReport']
-            if debugReport == None:
-                debugReport = 0
-
-            # 烧录报表的数量
-            burningReport = (burning.objects.filter(is_delete=False, work_order=workId)
-                             .aggregate(burningReport=Sum('quantity')))['burningReport']
-            if burningReport == None:
-                burningReport = 0
-
-            # 维修报表的数量
-            repairReport = (Dict.objects.filter(is_delete=False, work_order=workId)
-                            .aggregate(repairReport=Sum('bad_number')))['repairReport']
-            if repairReport == None:
-                repairReport = 0
-            deliveryDate = shipmentReport.delivery_date.strftime("%Y-%m-%d")
-            if deliveryDate == None:
-                deliveryDate = 'None'
-
-            startDate = shipmentReport.order_date.strftime("%Y-%m-%d")
-            if startDate == None:
-                startDate = 'None'
-
-
-            Alldata = {
-                'work_order_id': workId,
-                'name': shipmentReport.client_name,
-                'model': shipmentReport.shape,
-                'deliveryDate': deliveryDate,
-                'startDate': startDate,
-                'endDate': shipmentReport.finish_date,
-                'product_count':shipmentReport.product_count,
-
-                'inspect_quantity':inspectReport, #质检数量
-                'debug_quantity':debugReport, #调试数量
-                'burning_quantity':burningReport,  #烧录数量
-                'repair_quantity':repairReport,  #维修数量
+    producerecord_list = paginator.page(page)
+    # 实例化结果
+    result = []
+    # 遍历数据源
+    if len(producerecord_list) > 0 and len(producerecord_list) < 20:
+        for item in producerecord_list:
+            item.rate = int(item.repair_total/item.total)
+            data = {
+                'product_name': item.name,
+                'repair_total': item.repair_total,
+                'total': item.total,
+                'rate': item.rate,
             }
-            data.add(tuple(Alldata.items()))
-    SendData = [dict(item) for item in data]
-    # print(f"根据工单查询所需要的数据{SendData}")
-    return R.ok(data=SendData, count=count)
+            result.append(data)
+    # 返回结果
+    return R.ok(data=result, count=count)
 
-def getShipmentData():
-    shipmentReportS = Shipment.objects.filter(is_delete=False)
-    if not shipmentReportS:
-        return None
-    data = set()
-    if len(shipmentReportS) > 0 or len(shipmentReportS) < 20:
-        for shipmentReport in shipmentReportS:
-            Alldata = {
-                'name': shipmentReport.client_name,
-                'product_count': shipmentReport.product_count,
+
+def ResultData(request):
+    startTime = request.GET.get('startTime')
+    endTime = request.GET.get('endTime')
+    if startTime and endTime:
+        startTime = startTime.replace("+", " ")
+        endTime = endTime.replace("+", " ")
+        # sql = 'SELECT item_number,sum(examine_an_amount) AS total,sum(examine_a_bad_amount) AS badtotal FROM django_inspectreport WHERE is_delete = 0 AND start_time >= ' + str(startTime) + ' AND end_time <= ' + str(endTime)+ " GROUP BY item_number" + " limit " + str(limit)
+        sql = ("SELECT id, analysis, count(analysis) AS num "
+               "FROM django_repairreport "
+               "WHERE is_delete = 0  AND DATE(create_time) >= %s AND DATE(create_time) <= %s "
+               "GROUP BY analysis ")
+        query = Dict.objects.raw(sql, [startTime, endTime])
+
+    else:
+        sql =("SELECT id, analysis, count(analysis) AS num "
+               "FROM django_repairreport "
+               "WHERE is_delete = 0  "
+               "GROUP BY analysis ")
+        query = Dict.objects.raw(sql)
+    # 实例化结果
+    result = []
+    # 遍历数据源
+    if len(query) > 0 :
+        for item in query:
+            data = {
+                'analysis': item.analysis,
+                'num': item.num,
             }
-            data.add(tuple(Alldata.items()))
-    SendData = [dict(item) for item in data]
-    # print(SendData)
-    return R.ok(data=SendData)
+            result.append(data)
+    # 返回结果
+    return R.ok(data=result)
