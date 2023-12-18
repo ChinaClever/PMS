0665236019bfd608c858ba32f9b633c6219dd4c4
luosuofan <luozhiyong131@qq.com>
2023-12-18 14:22:06 +0800

    修改出货报表为数据源,修改时间

--- pmsAdmin/application/analysis/services.py
+++ pmsAdmin/application/analysis/services.py
@@ -26,20 +26,22 @@ def RepairData(request):
         # sql = 'SELECT item_number,sum(examine_an_amount) AS total,sum(examine_a_bad_amount) AS badtotal FROM django_inspectreport WHERE is_delete = 0 AND start_time >= ' + str(startTime) + ' AND end_time <= ' + str(endTime)+ " GROUP BY item_number" + " limit " + str(limit)
         sql = ("SELECT t1.id, t1.name, sum(t1.repair_number) AS repair_total, sum(t2.examine_an_amount) AS total "
                "FROM django_repairreport AS t1 "
-               "Join django_inspectreport AS t2 ON t1.name = t2.product_name "
-               "WHERE t1.is_delete = 0 AND t2.is_delete=0 and DATE(create_time) >= %s AND DATE(create_time) <= %s "
+               "Join django_shipmentreport AS t2 ON t1.name = t2.product_name "
+               "WHERE t1.is_delete = 0 AND t2.is_delete=0 and t1.repair_time >= %s AND t1.repair_time <= %s t2.order_date >= %s AND t2.order_date <= %s "
                "GROUP BY name ")
         query = Dict.objects.raw(sql, [startTime, endTime])
         # 设置分页
         paginator = Paginator(query, limit)
     else:
+        endTime = datetime.now().date()
+        startTime = endTime - timedelta(days=365)
 
         sql = ("SELECT t1.id, t1.name, sum(t1.repair_number) AS repair_total, sum(t2.examine_an_amount) AS total "
                "FROM django_repairreport AS t1 "
-               "Join django_inspectreport AS t2 ON t1.name = t2.product_name "
-               "WHERE t1.is_delete = 0 and t2.is_delete = 0 "
+               "Join django_shipmentreport AS t2 ON t1.name = t2.product_name "
+               "WHERE t1.is_delete = 0 and t2.is_delete = 0 and t1.repair_time >= %s AND t1.repair_time <= %s t2.order_date >= %s AND t2.order_date <= %s "
                "GROUP BY name ")
-        query = Dict.objects.raw(sql)
+        query = Dict.objects.raw(sql,[startTime,endTime])
         paginator = Paginator(query, limit)
 
     # 记录总数
@@ -65,15 +67,14 @@ def RepairData(request):
 
 
 def ResultData(request):
-    startTime = request.GET.get('startTime')
-    endTime = request.GET.get('endTime')
+
+    endTime = datetime.now().date()
+    startTime = endTime-timedelta(days=365)
     if startTime and endTime:
-        startTime = startTime.replace("+", " ")
-        endTime = endTime.replace("+", " ")
         # sql = 'SELECT item_number,sum(examine_an_amount) AS total,sum(examine_a_bad_amount) AS badtotal FROM django_inspectreport WHERE is_delete = 0 AND start_time >= ' + str(startTime) + ' AND end_time <= ' + str(endTime)+ " GROUP BY item_number" + " limit " + str(limit)
         sql = ("SELECT id, analysis, count(analysis) AS num "
                "FROM django_repairreport "
-               "WHERE is_delete = 0  AND DATE(create_time) >= %s AND DATE(create_time) <= %s "
+               "WHERE is_delete = 0  AND repair_time >= %s AND repair_time <= %s "
                "GROUP BY analysis ")
         query = Dict.objects.raw(sql, [startTime, endTime])
 
