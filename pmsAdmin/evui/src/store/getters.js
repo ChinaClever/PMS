a215d10db96082539eb9ab502d96bff980210b71
luozhiyong <luozhiyong131@qq.com>
2023-12-22 09:52:14 +0800

    重新添加更改
    Signed-off-by: luozhiyong <luozhiyong131@qq.com>

--- pmsAdmin/evui/src/store/getters.js
+++
@@ -1,6 +0,0 @@
-export default {
-  user: state => state.user,
-  theme: state => state.theme,
-  permission: state => state.user.permission,
-  isLeaveTestdata: state=> state.routerchange.isLeaveTestdata,
-}
